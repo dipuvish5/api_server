f0db5b94189b16d1ba67b78eb621e21372ea8913
Deepak Vishwakarma <deepakvishwakarma@Deepaks-MacBook-Air.local>
2024-02-17 12:42:19 +0530

    Updated code deploy scripts

--- scripts/code_deploy/after_install.sh
+++ scripts/code_deploy/after_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Pull the latest version of the Docker image from Docker Hub
+echo Logging in to Docker Hub...
+docker login --username dipuvish5 --password deepakv123
 docker pull nest_api_server:latest
